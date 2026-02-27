f4badc4e3602cc4659d1debf636f97c84a451c6b
ChrisEsqui72 <esquibelchristopher72@gmail.com>
2026-02-26 20:20:34 -0700

    Initial commit, basic alctoken done, ready for INT testing

--- tree.h
+++ tree.h
@@ -6,3 +6,14 @@ struct tree
         struct tree *kids[10]; /* if nkids >0 */
         struct token *leaf;    /* if nkids == 0; NULL for ε productions */
 };
+
+struct token
+{
+    int category;   /* the integer code returned by yylex */
+    char *text;     /* the actual string (lexeme) matched */
+    int lineno;     /* the line number on which the token occurs */
+    char *filename; /* the source file in which the token occurs */
+    int ival;       /* for integer constants, store binary value here */
+    double dval;    /* for real constants, store binary value here */
+    char *sval;     /* for string constants, malloc space, de-escape, store the string (less quotes and after escapes) here */
+};
