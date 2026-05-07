5ae7e9040b2756e8437b1e62840a607f73de1d72
jbujold <jaxenbujy@DESKTOP-GIJS145.localdomain>
2026-05-07 10:57:14 -0600

    added more to hello_world test file to show more functionality

--- hello_world.kt
+++ hello_world.kt
@@ -1,17 +1,23 @@
 fun main() {
     var x: Int = 5;
     var y: Int = 3;
+    var z: Int = add(x,y);
+
+    println("z: $z");
 
     if (x > y) {
-        println("${x}");
-        println("x is bigger");
+        println("x is bigger than y");
     }
     else {
-        println("$x");
-        println("x is smaller");
+        println("x is smaller than y");
     }
 
-    for (i in 1..3) {
-        println("$i");
+    while (x < 10) {
+        println("x: $x");
+        x = x + 1;
     }
+}
+
+fun add(num1: Int, num2: Int): Int {
+    return num1 + num2;
 }
